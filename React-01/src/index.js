f289f2976fc5a0f3dc5cdca2c10512b666e261b1
Fei <fei.zhou.se@outlook.com>
2022-02-09 16:22:55 -0600

    Webpack bundler

--- React-01/src/index.js
+++ React-01/src/index.js
@@ -1,1 +1,11 @@
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom";
+import Logo from "./Logo";
+
+ReactDOM.render(
+  <>
+    <Logo />
+    <h1>Hello, world!</h1>
+  </>,
+  document.getElementById("root")
+);
