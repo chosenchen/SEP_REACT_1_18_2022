5f33dbac41dacb553ae37d556bff0af3b0696d7d
Catalina <catalinamcquade@gmail.com>
2022-02-10 12:41:48 -0500

    feat: Added .babelrc file

--- react-boilerplate/lib/index.js
+++ react-boilerplate/lib/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-console.log('hello world!'); // class Boilerplate extends React.Component {
-//   render() {
-//     console.log('hello!');
-//     // return <h1>Hello!</h1>;
-//   }
-// }
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_reactDom["default"].render( /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h3", null, "React Boilerplate")), document.getElementById('main'));
