c5ab7689f7663140b3fb6bdfe14ad5ceae6cc7dd
Xiang Liu <jane@Xiangs-MacBook-Pro.local>
2022-02-09 23:26:55 -0800

    Update bundler

---
+++ React_1/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Logo from "./Logo";
+
+ReactDOM.render(
+  <>
+    <Logo />
+    <h1>Hello, world!</h1>
+  </>,
+  document.getElementById("root")
+);
