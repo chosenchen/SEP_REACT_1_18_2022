59fb085a189e20b08de46f9ec79c5e11d07bbfb0
Jenny Yang <89806167+Jenny019@users.noreply.github.com>
2022-02-10 22:39:12 -0500

    Submit by directly drag files(Just a try)

---
+++ My-Boilterplate/src/index.js
@@ -0,0 +1,7 @@
+// console.log("Hello My Friends");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
