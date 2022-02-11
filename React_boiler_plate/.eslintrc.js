045c5d46e897a89e8d9d20902d84e198b1549e09
louis09392003 <louis09392003@gmail.com>
2022-02-10 19:52:42 -0700

    eslint

---
+++ React_boiler_plate/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      'import/extensions': 'off',
+      'no-console': 'off',
+      'react/prop-types': 0,
+      'no-alert': 'off',
+    },
+  };
