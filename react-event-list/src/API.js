35f5dc583827e56d224dd9fd7d3b2540ec0229cc
Trey Fleshman <82164002+TreyFleshman@users.noreply.github.com>
2022-02-11 17:28:10 -0500

    Created State

--- react-event-list/src/API.js
+++ react-event-list/src/API.js
@@ -1,1 +1,11 @@
+const url = "http://localhost:4000";
 
+const getEvents = () =>
+  fetch([url])
+  .then((response) => response.json());
+
+const API = {
+  getEvents
+};
+
+export default API;
--- react-event-list/src/events.js
+++ react-event-list/src/events.js
@@ -1,40 +1,44 @@
 import React from 'react';
 import $ from 'jquery'
 import events from './db.json'
+import API from './API'
+import EventRow from './EventRow';
 
 events = events.events;
 
 events.forEach(ele => {
   ele.startDate = new Date(+ele.startDate);
   ele.endDate = new Date(+ele.endDate);
-  ele.startDate = ele.startDate.getFullYear() + '-' + (ele.startDate.getMonth() + 1) + '-' +  (ele.startDate.getDate()+ 1);
-  ele.endDate = ele.endDate.getFullYear() + '-' + (ele.endDate.getMonth() + 1)  + '-' +  (ele.endDate.getDate()+ 1);
-  ele.startDate = ele.startDate.split(/\D/).slice(0,3).map(num=>num.padStart(2,"0")).join("-");
-  ele.endDate = ele.endDate.split(/\D/).slice(0,3).map(num=>num.padStart(2,"0")).join("-");
+  ele.startDate = ele.startDate.getFullYear() + '-' + (ele.startDate.getMonth() + 1) + '-' + (ele.startDate.getDate() + 1);
+  ele.endDate = ele.endDate.getFullYear() + '-' + (ele.endDate.getMonth() + 1) + '-' + (ele.endDate.getDate() + 1);
+  ele.startDate = ele.startDate.split(/\D/).slice(0, 3).map(num => num.padStart(2, "0")).join("-");
+  ele.endDate = ele.endDate.split(/\D/).slice(0, 3).map(num => num.padStart(2, "0")).join("-");
 });
 
+
+
 class EventList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { ...props, events: [] }
+    this.createRow = this.createRow.bind(this);
+  }
+
+  getEventList() {
+    const events = API.getEvents();
+    this.setState({ events: events });
+  }
+
+  createRow() {
+    return this.state.events.map(event => {
+      <EventRow event={event} key={event.id}/>
+    })
+  }
+  
   render() {
     return (
-      events.map(event => (
-        <tbody id="event__container" key={event.id}>
-          <tr className="event_display_container">
-            <td className="event_display_name">
-              <input disabled id="event__name" value={event.eventName} />
-            </td>
-            <td>
-              <input disabled type="date" id="event__start__date" value={event.startDate} />
-            </td>
-            <td>
-              <input disabled type="date" id="event__end__date" value={event.endDate} />
-            </td>
-            <td>
-              <button className="btn edit__btn" id="edit__btn" name="edit" value={event.id}>EDIT</button>
-              <button className="btn del__btn" name="delete" id={event.id}>DELETE</button>
-            </td>
-          </tr>
-        </tbody>
-      ))
+      <EventRow />
     )
   }
 }
