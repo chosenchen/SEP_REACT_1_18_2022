dda016fdc354598836a5d6a645e2649046d7427d
Filamento <marketing@filamento.com>
2022-02-10 21:29:52 -0800

    Finish HTMLPlugIn Boilerplate

---
+++ React_Boilerplate_HtmlPlugIn/src/index.js
@@ -0,0 +1,18 @@
+import Logo from './Logo'
+
+class HelloMessage extends React.Component {
+    state = {name: 'Xiang Liu Antra Test'}
+    render() {
+      return (
+        <div>
+          <Logo></Logo>Hello {this.props.name} from {this.state.name}
+        </div>
+      );
+    }
+  }
+  
+  
+  ReactDOM.render(
+    <HelloMessage name="Taylor" />,
+    document.getElementById('hello-example')
+  );
---
+++ React_Boilerplate_Loaders/src/index2.js
@@ -0,0 +1,10 @@
+const a = { number: [1, 2, 3] }
+
+class App {
+    state = {
+        name: 'Jane'
+    }
+}
+
+console.log(new App())
+
