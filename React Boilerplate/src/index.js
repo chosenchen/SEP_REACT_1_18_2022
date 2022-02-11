a28069ead87cef16a2bcfd1506f0d21335848559
Stephen Wuh <swuh87@gmail.com>
2022-02-11 13:57:07 -0500

    initial commit

--- React Boilerplate/src/index.js
+++ React Boilerplate/src/index.js
@@ -4,11 +4,22 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import Logo from './Logo';
 
+class Random extends React.Component {
+  // This should be in the render function:
+  n = Math.floor(Math.random() * 10 + 1);
+ 
+  render() {
+    return <h1>The number is {this.n}!</h1>;
+  }
+};
+
 class HelloMessage extends React.Component {
+  
   state = { name: 'Stephen' };
   render() {
     return (
       <div className="container">
+        <Random/>
         <Logo></Logo> Hello {this.props.name} From {this.state.name}
       </div>
     );
