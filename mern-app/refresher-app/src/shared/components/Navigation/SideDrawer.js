d2298b5c7af519824312c62684b039c6177536ff
lilizhang <lilizhang254@gmail.com>
2022-02-16 22:04:49 -0800

    create portal for side-drawer

--- mern-app/refresher-app/src/shared/components/Navigation/SideDrawer.js
+++ mern-app/refresher-app/src/shared/components/Navigation/SideDrawer.js
@@ -1,12 +1,10 @@
 import React from "react";
-
+import ReactDOM from 'react-dom'
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-    console.log(props.children);
-    return (
-        <aside className="side-drawer">{props.children}</aside>
-    )
+    const content = <aside className="side-drawer">{props.children}</aside>;
+    return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 }
 
 export default SideDrawer;
