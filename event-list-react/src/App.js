06043034d7aa2382cccddc429f83849ca53acf22
Khoa Nguyen <nkhoa0101@yahoo.com>
2022-02-18 12:30:00 -0800

    Add header for app change

--- event-list-react/src/App.js
+++ event-list-react/src/App.js
@@ -1,11 +1,36 @@
+import React from 'react';
 import EventIndex from './Events/event_index';
+import Header from './Events/header';
 
-function App() {
-  return (
-    <div className="App">
-      <EventIndex/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      eventIndex: true,
+      eventUpcoming: false,
+    }
+  }
+
+  setCurrentApp = () => {
+    this.setState({
+      eventIndex : !this.state.eventIndex,
+      eventUpcoming : !this.state.eventUpcoming
+    });
+  }
+
+  render () {
+    const {eventIndex, eventUpcoming} = this.state;
+    return (
+      <div className="App">
+        <Header 
+        eventIndex={eventIndex}
+        eventUpcoming={eventUpcoming}
+        setCurrentApp={this.setCurrentApp}
+        />
+        {eventIndex && <EventIndex/>}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ event-list-react/src/Events/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Header extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const {setCurrentApp, eventIndex, eventUpcoming} = this.props;
+    return (
+      <div>
+        <div className='header-container'>
+          <button className='event-btn' onClick={setCurrentApp} disabled={eventIndex}>Event Manager</button>
+          <button className='event-btn' onClick={setCurrentApp} disabled={eventUpcoming}>Upcoming Events</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header;
