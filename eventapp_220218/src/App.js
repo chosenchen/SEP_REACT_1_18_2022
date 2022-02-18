0a5e1c1c8e34fc6047562898267b3db2c3ca3e5f
Xiang Liu <jane@Xiangs-MacBook-Pro.local>
2022-02-18 13:58:42 -0800

    Add Coming Events Component

--- eventapp_220218/src/App.js
+++ eventapp_220218/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import EventApp from './components/EventApp/EventApp';
+import ComingEvent from './components/ComingEvent/ComingEvent';
 import './App.css';
 
 function App() {
+ const [showEvent, setshowEvent] = useState(true)
   return (
-    <div className="App">
-      {/* <Header></Header> */}
-      <EventApp />
-      {/* <ComingEvent/> */}
-    </div>
+    <section>
+      <header class="navbar">
+        <div onClick={()=>setshowEvent(true)}>EventApp</div>
+        <div  onClick={()=>setshowEvent(false)}>ComingEvent</div>
+      </header>
+
+      <section className="App">
+       { showEvent ? <EventApp /> :  
+       <ComingEvent />}
+          
+      </section>
+    </section>
   );
 }
 
---
+++ eventapp_220218/src/components/ComingEvent/ComingEvent.js
@@ -0,0 +1,225 @@
+import React from 'react';
+import './ComingEvent.css';
+import {
+  getAllEvents,
+  addNewEvent,
+  deleteEvent,
+  editEvent,
+} from '../../services/event.api';
+
+import { EventData } from '../../models/EventData';
+import EventEditRow from '../EventEditRow/EventEditRow';
+import EventRow from '../EventRow/EventRow';
+import EventAddRow from '../EventAddRow/EventAddRow';
+import EventDataRow from '../EventDataRow/EventDataRow';
+import Button from '../Button/Button';
+
+class CommingEvent extends React.Component {
+  state = {
+    events: [],
+    dataCol: ['Event Name', 'Start Date', 'End Date', ''],
+    isShowAddEventRow: false,
+    newEvent: new EventData('', '' + Date.now(), '' + Date.now()),
+  };
+
+  generateEditEventstate = (event) => {
+    event.isEditing = false;
+    event.editEvent = new EventData(
+      event.eventName,
+      event.startDate,
+      event.endDate,
+      event.id
+    );
+  };
+
+  fetchAllEvents = () => {
+    getAllEvents().then((data) => {
+      const events = data.map(({ eventName, startDate, endDate, id }) => {
+        const newEvent = new EventData(eventName, startDate, endDate, id);
+        this.generateEditEventstate(newEvent);
+        return newEvent;
+      });
+
+      this.setState({
+        events,
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.fetchAllEvents();
+  }
+
+  hanldeAddEvent = () => {
+    this.setState({
+      isShowAddEventRow: true,
+    });
+  };
+  hanldeOnChange = ({ target: { name, value } }) => {
+    this.setState({
+      newEvent: {
+        ...this.state.newEvent,
+        [name]: value,
+      },
+    });
+  };
+
+  hanldeDelete = (id) => {
+    deleteEvent(id)
+      .then(() => {
+        this.fetchAllEvents();
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  };
+
+  handleClose = () => {
+    this.setState({
+      isShowAddEventRow: false,
+      newEvent: new EventData('', '' + Date.now(), '' + Date.now()),
+    });
+  };
+
+  hanldeSaveAddNew = () => {
+    const { eventName, startDate, endDate } = this.state.newEvent;
+    const newEvent = new EventData(eventName, startDate, endDate);
+    newEvent.parseTimeStamp();
+    if (newEvent.isValidForSave()) {
+      addNewEvent(newEvent).then((data) => {
+        //this.setState({ events: [data, ...this.state.events] });
+        this.fetchAllEvents();
+      });
+      this.handleClose();
+    } else {
+      alert('inValid');
+    }
+  };
+  hanldeEdit = ({ id }) => {
+    console.log('edit', id);
+    this.setState({
+      events: this.state.events.map((event) => {
+        if (event.id === id) {
+          return { ...event, isEditing: true };
+        } else {
+          return event;
+        }
+      }),
+    });
+  };
+
+  hanldeOnChangeEdit = ({ target: { name, value } }, { id }) => {
+    this.setState({
+      events: this.state.events.map((event) => {
+        if (event.id === id) {
+          return { ...event, editEvent: { ...event.editEvent, [name]: value } };
+        } else {
+          return event;
+        }
+      }),
+    });
+  };
+
+  hanldeCancel = (id) => {
+    this.setState({
+      events: this.state.events.map((event) => {
+        if (event.id === id) {
+          return { ...event, isEditing: false };
+        } else {
+          return event;
+        }
+      }),
+    });
+  };
+  hanldeEditSave = (editEventObj) => {
+    editEvent(editEventObj).then((data) => {
+      this.setState({
+        events: this.state.events.map((event) => {
+          if (event.id === editEventObj.id) {
+            return {
+              ...editEventObj,
+              isEditing: false,
+            };
+          } else {
+            return event;
+          }
+        }),
+      });
+    });
+  };
+
+  render() {
+    return (
+      <section className="event-app">
+        <header className="event-app__header">
+          Coming Events
+        </header>
+        <table className="event-app__table">
+          <thead>
+            <tr>
+              {this.state.dataCol?.map((col, index) => (
+                <th key={`${col}`}>{col}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.events?.map((event) =>
+              event.isEditing ? (
+                <EventDataRow
+                  key={event.id}
+                  event={event.editEvent}
+                  actions={[
+                    {
+                      actionName: 'Save',
+                      actionFn: this.hanldeEditSave,
+                    },
+                    {
+                      actionName: 'Cancel',
+                      actionFn: this.hanldeCancel,
+                    },
+                  ]}
+                  handleOnchange={this.hanldeOnChangeEdit}
+                ></EventDataRow>
+              ) : (
+                <EventDataRow
+                  key={event.id}
+                  event={event}
+                  actions={[
+                    {
+                      actionName: 'Edit',
+                      actionFn: this.hanldeEdit,
+                    },
+                    {
+                      actionName: 'Delete',
+                      actionFn: this.hanldeDelete,
+                    },
+                  ]}
+                ></EventDataRow>
+              )
+            )}
+          </tbody>
+          <tfoot>
+            {this.state.isShowAddEventRow ? (
+              <EventDataRow
+                event={this.state.newEvent}
+                actions={[
+                  {
+                    actionName: 'Save',
+                    actionFn: this.hanldeSaveAddNew,
+                  },
+                  {
+                    actionName: 'Close',
+                    actionFn: this.handleClose,
+                  },
+                ]}
+                handleOnchange={this.hanldeOnChange}
+              ></EventDataRow>
+            ) : null}
+          </tfoot>
+        </table>
+      </section>
+    );
+  }
+}
+
+export default CommingEvent;
