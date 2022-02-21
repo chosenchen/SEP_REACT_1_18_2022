b16ae5b20ebee209ce333894be34636d103218ae
Fei <fei.zhou.se@outlook.com>
2022-02-21 15:25:09 -0600

    class component v1.1

--- event-list-react/src/components/ComingEventList/index.js
+++ event-list-react/src/components/ComingEventList/index.js
@@ -1,40 +1,44 @@
+import React from "react";
 import EventListRow from "../EventListRow";
 
-const ComingEventList = ({ eventList }) => {
-  let eventListJSX;
+class ComingEventList extends React.Component {
+  render() {
+    const { eventList } = this.props;
+    let eventListJSX;
 
-  if (eventList.length) {
-    eventListJSX = eventList.map((eventItem) => {
-      return (
-        <EventListRow
-          key={eventItem.id}
-          eventItem={eventItem}
-          hasActions={false}
-        />
-      );
-    });
-  } else {
-    eventListJSX = <h1>Empty List</h1>;
-  }
+    if (eventList.length) {
+      eventListJSX = eventList.map((eventItem) => {
+        return (
+          <EventListRow
+            key={eventItem.id}
+            eventItem={eventItem}
+            hasActions={false}
+          />
+        );
+      });
+    } else {
+      eventListJSX = <h1>Empty List</h1>;
+    }
 
-  return (
-    <section className="container">
-      <section className="eventlist__app card">
-        <div className="eventlist__content">
-          <header className="eventlist__header">
-            <ul className="eventlist__head">
-              <li>Event Name</li>
-              <li>Start Date</li>
-              <li>End Date</li>
-            </ul>
-          </header>
-          <div className="eventlist__body" id="comingeventlist_container">
-            {eventListJSX}
+    return (
+      <section className="container">
+        <section className="eventlist__app card">
+          <div className="eventlist__content">
+            <header className="eventlist__header">
+              <ul className="eventlist__head">
+                <li>Event Name</li>
+                <li>Start Date</li>
+                <li>End Date</li>
+              </ul>
+            </header>
+            <div className="eventlist__body" id="comingeventlist_container">
+              {eventListJSX}
+            </div>
           </div>
-        </div>
+        </section>
       </section>
-    </section>
-  );
-};
+    );
+  }
+}
 
 export default ComingEventList;
--- event-list-react/src/containers/EventListContainer/index.js
+++ event-list-react/src/containers/EventListContainer/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState, useEffect } from "react";
 
 import API from "../../api";
 import Header from "../../components/Header";
