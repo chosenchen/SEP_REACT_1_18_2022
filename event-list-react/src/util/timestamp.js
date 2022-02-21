ef434c4c952be1cfb6f2d4e4f39f03581b0d3846
Fei <fei.zhou.se@outlook.com>
2022-02-21 17:07:39 -0600

    HOC

--- event-list-react/src/util/timestamp.js
+++ event-list-react/src/util/timestamp.js
@@ -1,23 +1,34 @@
 export const timestampToStr = (timestamp) => {
-  const date = new Date(+timestamp);
+  // const date = new Date(+timestamp);
 
-  const yearStr = `${date.getFullYear()}`;
+  // const yearStr = `${date.getFullYear()}`;
 
-  let monthStr = `${+date.getMonth() + 1}`;
-  if (monthStr.length === 1) {
-    monthStr = "0" + monthStr;
+  // let monthStr = `${+date.getMonth() + 1}`;
+  // if (monthStr.length === 1) {
+  //   monthStr = "0" + monthStr;
+  // }
+
+  // let dayStr = `${date.getDate()}`;
+  // if (dayStr.length === 1) {
+  //   dayStr = "0" + dayStr;
+  // }
+
+  // return `${yearStr}-${monthStr}-${dayStr}`;
+
+  try {
+    return new Date(+timestamp).toISOString().substring(0, 10);
+  } catch {
+    return "";
   }
-
-  let dayStr = `${date.getDate()}`;
-  if (dayStr.length === 1) {
-    dayStr = "0" + dayStr;
-  }
-
-  return `${yearStr}-${monthStr}-${dayStr}`;
 };
 
 export const dateStrToTimestamp = (dateStr) => {
-  const timestamp = new Date(dateStr);
+  // const timestamp = new Date(dateStr);
+  // return timestamp.valueOf();
 
-  return timestamp.valueOf();
+  try {
+    return "" + new Date(dateStr).getTime();
+  } catch {
+    return "";
+  }
 };
