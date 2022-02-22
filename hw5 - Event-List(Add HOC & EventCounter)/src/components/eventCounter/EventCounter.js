8b3fb1834c15a3ba6a2132247c6949026784c1aa
Jenny Yang <89806167+Jenny019@users.noreply.github.com>
2022-02-22 02:11:50 -0500

    Delete redundancy

--- hw5 - Event-List(Add HOC & EventCounter)/src/components/eventCounter/EventCounter.js
+++ hw5 - Event-List(Add HOC & EventCounter)/src/components/eventCounter/EventCounter.js
@@ -2,14 +2,6 @@ import React from "react";
 
 import withEventData from "../HOC/withEventData";
 
-// class EventCounter extends React.Component {
-//     render() {
-//         return (
-//             <h4>Counter: {this.props.eventList.length}</h4>
-//         )
-//     }
-// }
-
 class EventCounter extends React.Component {
     render() {
         return (
