99c643207af8699011ea315c6373d3567d66aaed
Catalina <catalinamcquade@gmail.com>
2022-02-23 13:25:25 -0500

    feat: EventAddRow component successfully converted from class to functional component

--- event-app-functional/src/components/EventAddRow/EventAddRow.js
+++ event-app-functional/src/components/EventAddRow/EventAddRow.js
@@ -1,43 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-class EventAddRow extends React.Component {
-  render() {
-    const { newEvent, hanldeOnChange, hanldeSaveAddNew, handleClose } =
-      this.props;
-    return (
-      <tr>
-        <td>
-          <input
-            type="text"
-            name="eventName"
-            value={newEvent.eventName}
-            onChange={hanldeOnChange}
-          />
-        </td>
-        <td>
-          <input
-            onChange={hanldeOnChange}
-            type="date"
-            value={newEvent.startDate}
-            name="startDate"
-          />
-        </td>
-        <td>
-          <input
-            onChange={hanldeOnChange}
-            name="endDate"
-            type="date"
-            value={newEvent.endDate}
-          />
-        </td>
-        <td>
-          <button onClick={hanldeSaveAddNew}>Save</button>
-          <button onClick={handleClose}>Close</button>
-        </td>
-      </tr>
-    );
-  }
-}
+
+const EventAddRow = ({
+  newEvent,
+  hanldeOnChange,
+  hanldeSaveAddNew,
+  handleClose,
+}) => {
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          name="eventName"
+          value={newEvent.eventName}
+          onChange={hanldeOnChange}
+        />
+      </td>
+      <td>
+        <input
+          onChange={hanldeOnChange}
+          type="date"
+          value={newEvent.startDate}
+          name="startDate"
+        />
+      </td>
+      <td>
+        <input
+          onChange={hanldeOnChange}
+          name="endDate"
+          type="date"
+          value={newEvent.endDate}
+        />
+      </td>
+      <td>
+        <button onClick={hanldeSaveAddNew}>Save</button>
+        <button onClick={handleClose}>Close</button>
+      </td>
+    </tr>
+  );
+};
 
 EventAddRow.propTypes = {
   newEvent: PropTypes.object,
