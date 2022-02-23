d3daf6d159231e895688d00b6f0784f25bc0e9a0
Fei <fei.zhou.se@outlook.com>
2022-02-22 21:22:49 -0600

    API optimization

--- eventapp/src/services/event.api.js
+++ eventapp/src/services/event.api.js
@@ -1,102 +1,104 @@
-const baseURL = "http://localhost:9000";
-const path = "events";
-
-export const getAllEvents = (signal) =>
-  fetch([baseURL, path].join("/"), {
-    method: "GET",
-    signal,
-  }).then(
-    (response) =>
-      new Promise((res, rej) => {
-        setTimeout(() => {
-          res(response.json());
-        }, 2000);
-      })
-  );
-
-export const addNewEvent = (newEvent, signal) =>
-  fetch([baseURL, path].join("/"), {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify(newEvent),
-    signal,
-  }).then((response) => response.json());
-
-export const deleteEvent = (event, signal) =>
-  fetch([baseURL, path, event.id].join("/"), {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    signal,
-  }).then((response) => response.json());
-
-export const editEvent = (editEvent, signal) =>
-  fetch([baseURL, path, editEvent.id].join("/"), {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify(editEvent),
-    signal,
-  }).then((response) => response.json());
-
 // const baseURL = "http://localhost:9000";
 // const path = "events";
 
-// const request = async (url, option) => {
-//   // config
-//   const defaultOption = {
-//     "Content-Type": "application/json",
-//     Accept: "application/json",
-//   };
-
-//   const { signal, ...restOptions } = option;
-
-//   let fetchOption;
-//   if (signal) {
-//     fetchOption = {
-//       ...defaultOption,
-//       ...restOptions,
-//       signal,
-//     };
-//   } else {
-//     fetchOption = { ...defaultOption, ...restOptions };
-//   }
-
-//   console.log(fetchOption);
-
-//   return fetch(url, fetchOption).then(
+// export const getAllEvents = (signal) =>
+//   fetch([baseURL, path].join("/"), {
+//     method: "GET",
+//     signal,
+//   }).then(
 //     (response) =>
-//       new Promise((resolve, reject) => {
+//       new Promise((res, rej) => {
 //         setTimeout(() => {
-//           resolve(response.json());
-//         }, 1);
+//           res(response.json());
+//         }, 2000);
 //       })
 //   );
-// };
 
-// export const getAllEvents = (signal = null) =>
-//   request([baseURL, path].join("/"), { method: "GET", signal });
-
-// export const addNewEvent = (newEvent, signal = null) =>
-//   request([baseURL, path].join("/"), {
+// export const addNewEvent = (newEvent, signal) =>
+//   fetch([baseURL, path].join("/"), {
 //     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
 //     body: JSON.stringify(newEvent),
 //     signal,
-//   });
+//   }).then((response) => response.json());
 
-// export const deleteEvent = (event, signal = null) =>
-//   request([baseURL, path, event.id].join("/"), { method: "DELETE", signal });
+// export const deleteEvent = (event, signal) =>
+//   fetch([baseURL, path, event.id].join("/"), {
+//     method: "DELETE",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     signal,
+//   }).then((response) => response.json());
 
-// export const editEvent = (editEvent, signal = null) =>
-//   request([baseURL, path, editEvent.id].join("/"), {
+// export const editEvent = (editEvent, signal) =>
+//   fetch([baseURL, path, editEvent.id].join("/"), {
 //     method: "PUT",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
 //     body: JSON.stringify(editEvent),
 //     signal,
-//   });
+//   }).then((response) => response.json());
+
+const baseURL = "http://localhost:9000";
+const path = "events";
+
+const request = async (url, option) => {
+  // config
+  const defaultOption = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  };
+
+  const { signal, ...restOptions } = option;
+
+  let fetchOption;
+  if (signal) {
+    fetchOption = {
+      ...defaultOption,
+      ...restOptions,
+      signal,
+    };
+  } else {
+    fetchOption = { ...defaultOption, ...restOptions };
+  }
+
+  console.log(fetchOption);
+
+  return fetch(url, fetchOption).then(
+    (response) =>
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(response.json());
+        }, 3000);
+      })
+  );
+};
+
+export const getAllEvents = (signal = null) =>
+  request([baseURL, path].join("/"), { method: "GET", signal });
+
+export const addNewEvent = (newEvent, signal = null) =>
+  request([baseURL, path].join("/"), {
+    method: "POST",
+    body: JSON.stringify(newEvent),
+    signal,
+  });
+
+export const deleteEvent = (event, signal = null) =>
+  request([baseURL, path, event.id].join("/"), { method: "DELETE", signal });
+
+export const editEvent = (editEvent, signal = null) =>
+  request([baseURL, path, editEvent.id].join("/"), {
+    method: "PUT",
+    body: JSON.stringify(editEvent),
+    signal,
+  });
