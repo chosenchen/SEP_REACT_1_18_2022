88c9f19c34659cb9e7722c158eae80ce606f54e5
Catalina <catalinamcquade@gmail.com>
2022-02-23 13:24:04 -0500

    feat: EventTable component successfully converted from class to functional component

--- event-app-functional/src/components/EventTable/EventTable.js
+++ event-app-functional/src/components/EventTable/EventTable.js
@@ -1,32 +1,28 @@
 import React from 'react';
 
-class EventTable extends React.Component {
-  render() {
-    const { renderHeader, dataCol, renderFooter } = this.props;
+const EventTable = ({ renderHeader, dataCol, renderFooter, children }) => {
+  const header = renderHeader ? (
+    <header className="event-app__header">{renderHeader()}</header>
+  ) : null;
+  const footer = renderFooter ? <tfoot>{renderFooter()}</tfoot> : null;
 
-    const header = renderHeader ? (
-      <header className="event-app__header">{renderHeader()}</header>
-    ) : null;
-    const footer = renderFooter ? <tfoot>{renderFooter()}</tfoot> : null;
+  return (
+    <section className="event-app">
+      {header}
 
-    return (
-      <section className="event-app">
-        {header}
-
-        <table className="event-app__table">
-          <thead>
-            <tr>
-              {dataCol?.map((col, index) => (
-                <th key={`${col}`}>{col}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>{this.props.children}</tbody>
-          {footer}
-        </table>
-      </section>
-    );
-  }
-}
+      <table className="event-app__table">
+        <thead>
+          <tr>
+            {dataCol?.map((col, index) => (
+              <th key={`${col}`}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>{children}</tbody>
+        {footer}
+      </table>
+    </section>
+  );
+};
 
 export default EventTable;
