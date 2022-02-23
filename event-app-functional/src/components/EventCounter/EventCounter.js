35584a55c1aef76007257de40cafa365502929a6
Catalina <catalinamcquade@gmail.com>
2022-02-23 13:27:33 -0500

    feat: EventCounter component successfully converted from class to functional component

--- event-app-functional/src/components/EventCounter/EventCounter.js
+++ event-app-functional/src/components/EventCounter/EventCounter.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { withEventData } from '../../hoc/withEventData';
-class EventCounter extends React.Component {
-  render() {
-    const { events } = this.props;
-    return <h1>{events.length}</h1>;
-  }
-}
+
+const EventCounter = ({ events }) => {
+  return <h1>{events.length}</h1>;
+};
 
 const EventCounterConnector = withEventData(EventCounter);
 
