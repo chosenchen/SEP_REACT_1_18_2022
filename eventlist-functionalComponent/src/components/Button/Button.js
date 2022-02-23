8691e47c798c9456106976d3960624c5aec0719d
shuxiegit config -- global user.email dorisxieshu@gmail.comssh-keygen -t rsa -C dorisxieshu@gmail.com <dorisxieshu@gmail.com>
2022-02-22 23:19:32 -0800

    render with functional components

---
+++ eventlist-functionalComponent/src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Button.css";
+
+// PureComponent
+function Button(props) {
+  const { children, ...restProps } = props;
+  return (
+    <button className="btn" {...restProps}>
+      {children}
+    </button>
+  );
+}
+// }
+
+export default Button;
---
+++ eventlist-functionalComponent/src/components/EventTable/EventTable.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function EventTable(props) {
+ 
+    const { renderHeader, dataCol, renderFooter } = props;
+
+    const header = renderHeader ? (
+      <header className="event-app__header">{renderHeader()}</header>
+    ) : null;
+    const footer = renderFooter ? <tfoot>{renderFooter()}</tfoot> : null;
+
+    return (
+      <section className="event-app">
+        {header}
+
+        <table className="event-app__table">
+          <thead>
+            <tr>
+              {dataCol?.map((col, index) => (
+                <th key={`${col}`}>{col}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>{props.children}</tbody>
+          {footer}
+        </table>
+      </section>
+    );
+  
+}
+
+export default EventTable;
