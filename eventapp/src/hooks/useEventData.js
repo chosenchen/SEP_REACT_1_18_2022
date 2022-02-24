01078f6876bb7992abd446a8909ea61454d2683b
Fei <fei.zhou.se@outlook.com>
2022-02-24 09:59:18 -0600

    redux wip

--- eventapp/src/hooks/useEventData.js
+++ eventapp/src/hooks/useEventData.js
@@ -7,12 +7,17 @@ import {
   editEvent,
 } from "../services/event.api";
 
+import store from "../store/appStore";
+
 import { EventData } from "../models/EventData";
 import { useAbort } from "./useAbort";
+import { string } from "prop-types";
 
 export const useEventData = () => {
   const { createSignal } = useAbort();
-  const [events, setEvents] = useState([]);
+  // const [events, setEvents] = useState([]);
+
+  const { events } = store.getState();
 
   useEffect(() => {
     fetchAllEvents();
@@ -40,7 +45,8 @@ export const useEventData = () => {
           return newEvent;
         });
 
-        setEvents(events);
+        // setEvents(events);
+        store.dispatch({ type: "event/get", payload: events });
       })
       .catch((err) => {
         console.warn(err);
@@ -51,16 +57,17 @@ export const useEventData = () => {
   const handleUpdateEvent = (updateEvent) => {
     return editEvent(updateEvent, createSignal())
       .then((data) => {
-        setEvents(
-          events.map((event) => {
-            if (event.id === data.id) {
-              console.log(events);
-              return { ...event, ...data };
-            } else {
-              return event;
-            }
-          })
-        );
+        store.dispatch({ type: "event/update", payload: data });
+
+        // setEvents(
+        //   events.map((event) => {
+        //   if (event.id === data.id) {
+        //     console.log(events);
+        //     return { ...event, ...data };
+        //   } else {
+        //     return event;
+        //   }
+        // }));
       })
       .catch((err) => {
         console.warn(err);
@@ -71,15 +78,16 @@ export const useEventData = () => {
   const handleDeleteEvent = (deletedEvent) => {
     return deleteEvent(deletedEvent, createSignal())
       .then((data) => {
-        setEvents(
-          events.filter((event) => {
-            if (event.id === deletedEvent.id) {
-              return false;
-            } else {
-              return true;
-            }
-          })
-        );
+        store.dispatch({ type: "event/delete", payload: deletedEvent });
+        // setEvents(
+        //   events.filter((event) => {
+        //     if (event.id === deletedEvent.id) {
+        //       return false;
+        //     } else {
+        //       return true;
+        //     }
+        //   })
+        // );
       })
       .catch((err) => {
         console.warn(err);
@@ -92,7 +100,9 @@ export const useEventData = () => {
       .then(({ eventName, startDate, endDate, id }) => {
         const newEvent = new EventData(eventName, startDate, endDate, id);
         generateEditEventstate(newEvent);
-        setEvents([...events, newEvent]);
+
+        store.dispatch({ type: "event/add", payload: newEvent });
+        // setEvents([...events, newEvent]);
       })
       .catch((err) => {
         console.warn(err);
@@ -101,31 +111,36 @@ export const useEventData = () => {
 
   // UI STATE
   const handleSetEdit = (setEditEvent, isEdit) => {
-    setEvents(
-      events.map((event) => {
-        if (event.id === setEditEvent.id) {
-          return { ...setEditEvent, isEditing: isEdit };
-        } else {
-          return event;
-        }
-      })
-    );
+    store.dispatch({
+      type: "event/setEdit",
+      payload: { setEditEvent, isEdit },
+    });
+    // setEvents(
+    //   events.map((event) => {
+    //     if (event.id === setEditEvent.id) {
+    //       return { ...setEditEvent, isEditing: isEdit };
+    //     } else {
+    //       return event;
+    //     }
+    //   })
+    // );
   };
 
   // UI STATE
   const handleOnChangeEditEvent = (editEvent) => {
-    setEvents(
-      events.map((event) => {
-        if (event.id === editEvent.id) {
-          return {
-            ...event,
-            editEvent: { ...editEvent },
-          };
-        } else {
-          return event;
-        }
-      })
-    );
+    store.dispatch({ type: "event/onChangeEditEvent", payload: editEvent });
+    // setEvents(
+    //   events.map((event) => {
+    //     if (event.id === editEvent.id) {
+    //       return {
+    //         ...event,
+    //         editEvent: { ...editEvent },
+    //       };
+    //     } else {
+    //       return event;
+    //     }
+    //   })
+    // );
   };
   return {
     events,
@@ -136,3 +151,5 @@ export const useEventData = () => {
     handleUpdateEvent,
   };
 };
+
+store.subscribe(useEventData);
