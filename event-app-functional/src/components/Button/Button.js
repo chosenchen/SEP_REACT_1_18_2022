c5625f4ba6d5485e152f4181ff7c354e2bf5ee2e
Catalina <catalinamcquade@gmail.com>
2022-02-23 21:05:35 -0500

    feat: Button component successfully converted from class to functional component

--- event-app-functional/src/components/Button/Button.js
+++ event-app-functional/src/components/Button/Button.js
@@ -2,19 +2,13 @@ import React from 'react';
 import './Button.css';
 
 // PureComponent
-class Button extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.children !== this.props.children;
-  }
-  render() {
-    console.log('Button render');
-    const { children, ...restProps } = this.props;
-    return (
-      <button className="btn" {...restProps}>
-        {children}
-      </button>
-    );
-  }
-}
+const Button = React.memo(({ children, ...restProps }) => {
+  console.log('Button render');
+  return (
+    <button className="btn" {...restProps}>
+      {children}
+    </button>
+  );
+});
 
 export default Button;
