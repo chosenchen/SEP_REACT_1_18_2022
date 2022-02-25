526052460dc6df2c68760353c8c9dbcdc175af01
Stephen Wuh <swuh87@gmail.com>
2022-02-25 10:12:54 -0500

    react redux provider deleted

--- React Event List _ Functional Component/src/index.js
+++ React Event List _ Functional Component/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./Redux/store.js";
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
