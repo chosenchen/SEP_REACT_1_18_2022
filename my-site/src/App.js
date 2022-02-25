e4fb07c000ae7cb82c0bb49604b8aabbad3a7406
Fei <fei.zhou.se@outlook.com>
2022-02-25 16:50:25 -0600

    Router with layout

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,21 +1,20 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import Header from "./portfolio/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./portfolio/Home/Home";
-import Footer from "./portfolio/Footer/Footer";
 import Contact from "./portfolio/Contact/Contact";
 import Project from "./portfolio/Project/Project";
+import PortfolioLayout from "./portfolio/PortfolioLayout/PortfolioLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/portfolio" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<PortfolioLayout />}>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="portfolio" element={<Project />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
       </Routes>
-      <Outlet />
-      <Footer />
     </BrowserRouter>
   );
 }
