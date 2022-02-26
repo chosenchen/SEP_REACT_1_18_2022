63ce73eb1411ce30da1aa251e88d6893b92b7478
Jenny Yang <89806167+Jenny019@users.noreply.github.com>
2022-02-26 15:59:29 -0500

    update components

---
+++ MySite/src/components/homePage/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Main from './Main';
+import About from './About';
+import Summary from './Summary';
+import Projects from './Projects';
+import Contact from './Contact';
+
+export default function Home() {
+  return (
+    <>
+      <Main />
+      <About />
+      <Summary />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
+
