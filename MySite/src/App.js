2e9bbc490a8ec395426590877662780b0697f2fa
Jenny Yang <89806167+Jenny019@users.noreply.github.com>
2022-02-26 16:00:58 -0500

    Update App.js

--- MySite/src/App.js
+++ MySite/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import NavigationBar from './components/navigationBar/NavigationBar';
-import MainBody from './components/mainBody/MainBody';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/homePage/Home';
+import Projects from './components/homePage/Projects';
+import Contact from './components/homePage/Contact';
+import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
-      <MainBody />
-      <Footer />       
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/portfolio' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
+      <Footer />  
     </div>
   );
 }
 
 export default App;
+
